ac9947cdb9b41e3d160fe13a04f950bd966aa739
Rohit Kumar Singh <100895939+rohit62043@users.noreply.github.com>
2023-03-17 15:26:14 +0530

    Adding solidity contracts

---
+++ Lottery.sol
@@ -0,0 +1,37 @@
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Lotery
+{
+    address public manager;
+    address payable[] public participants;
+
+    constructor()
+    {
+        manager=msg.sender;
+    }
+
+    receive() external payable
+    {
+        require(msg.value== 1 ether);
+        participants.push(payable(msg.sender));
+    }
+
+    function getBalance() public view returns(uint)
+    {
+        require(msg.sender==manager);
+        return address(this).balance;
+    }
+
+    function random() public view returns(uint){
+        return uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,participants.length)));
+    }
+
+    function selectWinner() public
+    {
+        require(msg.sender==manager);
+        require(participants.length>=3);
+        uint r=random();
+        uint index=r%participants.length;
+        participants[index].transfer(getBalance());
+    }
+}
